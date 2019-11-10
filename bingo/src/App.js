af665667609a3957719c8833f5d9c10c71314e3e
jross111 <jross.nyc111@gmail.com>
2019-11-10 03:30:52 -0500

    late night

--- bingo/src/App.js
+++ bingo/src/App.js
@@ -1,53 +1,18 @@
 import React from 'react';
 import './App.css';
-// import { Game } from './Game';
+import { Game } from './Game';
 // import { Game } from 'components/Game.js';
 
 function App() {
   return (
     <div className="App">
-    {/* <Game /> */}
-    < GameSquares />
+    <Game />
+
     </div>
   );
 }
 
 export default App;
 
-
-function GameSquares(){
-
-  return(
-    <div className="wrapper">
-      <div className="game-name">Bingo</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      <div className="item">X</div>
-      
-    </div>
-  )
-}
 
 
--- bingo/src/Board.js
+++ bingo/src/Board.js
@@ -1,51 +1,40 @@
 import React from 'react';
-import { Square } from "./Square";
-export class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-  render() {
-    const status = 'Bingo Generator';
-    return (<div>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-      </div>
-      <div className="board-row">
-        {this.renderSquare(5)}
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-        {this.renderSquare(9)}
-      </div>
-      <div className="board-row">
-        {this.renderSquare(10)}
-        {this.renderSquare(11)}
-        {this.renderSquare(12)}
-        {this.renderSquare(13)}
-        {this.renderSquare(14)}
-      </div>
-      <div className="board-row">
-        {this.renderSquare(15)}
-        {this.renderSquare(16)}
-        {this.renderSquare(17)}
-        {this.renderSquare(18)}
-        {this.renderSquare(19)}
-      </div>
-      <div className="board-row">
-        {this.renderSquare(20)}
-        {this.renderSquare(21)}
-        {this.renderSquare(22)}
-        {this.renderSquare(23)}
-        {this.renderSquare(24)}
-      </div>
-      <div className="board-row">
 
-      </div>
-    </div>);
-  }
+
+
+
+export function Board() {
+    const gameName = "Bingo"
+    return (
+
+        <div className="wrapper">
+
+            <div className="game-name">{gameName}</div>
+            <div className="item" id="1"></div>
+            <div className="item" id="2"></div>
+            <div className="item" id="3"></div>
+            <div className="item" id="4"></div>
+            <div className="item" id="5"></div>
+            <div className="item" id="6"></div>
+            <div className="item" id="7"></div>
+            <div className="item" id="8"></div>
+            <div className="item" id="9"></div>
+            <div className="item" id="10"></div>
+            <div className="item" id="11"></div>
+            <div className="item" id="12"></div>
+            <div className="item" id="13"></div>
+            <div className="item" id="14"></div>
+            <div className="item" id="15"></div>
+            <div className="item" id="16"></div>
+            <div className="item" id="17"></div>
+            <div className="item" id="18"></div>
+            <div className="item" id="19"></div>
+            <div className="item" id="20"></div>
+            <div className="item" id="21"></div>
+            <div className="item" id="22"></div>
+            <div className="item" id="23"></div>
+            <div className="item" id="24"></div>
+            <div className="item" id="25"></div>
+        </div>);
 }
+
