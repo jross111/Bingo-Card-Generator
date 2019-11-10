4f07b5ca21e5205c4e2b5f4e98274aad8d37b40e
jross111 <jross.nyc111@gmail.com>
2019-11-09 20:39:39 -0500

    changed game board to use CSS grid

--- bingo/src/App.js
+++ bingo/src/App.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import './App.css';
-import { Game } from './Game';
+// import { Game } from './Game';
 // import { Game } from 'components/Game.js';
 
 function App() {
   return (
     <div className="App">
-    <Game />
+    {/* <Game /> */}
+    < GameSquares />
     </div>
   );
 }
 
 export default App;
+
+
+function GameSquares(){
+
+  return(
+    <div className="wrapper">
+      <div className="game-name">Bingo</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      <div className="item">X</div>
+      
+    </div>
+  )
+}
 
 
